aee70fa9f5c0baf3d35ce90758c266c6670013de
ednilson <ednilsonpalhares@hotmail.com>
2020-02-13 22:57:47 -0300

    criando operação de instanciacao

--- src/main/java/com/ednilson/pedidos/SpsbApplication.java
+++ src/main/java/com/ednilson/pedidos/SpsbApplication.java
@@ -1,13 +1,31 @@
 package com.ednilson.pedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpsbApplication {
+import com.ednilson.pedidos.domain.Categoria;
+import com.ednilson.pedidos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpsbApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpsbApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
 }
