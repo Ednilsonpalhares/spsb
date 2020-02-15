8cb77d2fee73e4a1c1dcfd4c7cc369e9ae4f3117
ednilson <ednilsonpalhares@hotmail.com>
2020-02-15 20:23:53 -0300

    Adicionando a class Produto e assciando relacionamento com categooria

--- src/main/java/com/ednilson/pedidos/SpsbApplication.java
+++ src/main/java/com/ednilson/pedidos/SpsbApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ednilson.pedidos.domain.Categoria;
+import com.ednilson.pedidos.domain.Produto;
 import com.ednilson.pedidos.repositories.CategoriaRepository;
+import com.ednilson.pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpsbApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepo;
+	
+	@Autowired
+	private ProdutoRepository produtoRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpsbApplication.class, args);
@@ -25,7 +30,19 @@ public class SpsbApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepo.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepo.saveAll(Arrays.asList(p1,p2,p3));
 	}
 	
 }
---
+++ src/main/java/com/ednilson/pedidos/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.ednilson.pedidos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ednilson.pedidos.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	
+	
+}
