7149209421a80e88ff1a9d2e7e68ed886e64c9b7
ednilson <ednilsonpalhares@hotmail.com>
2020-02-19 05:32:20 -0300

    Adicionando Endpoint /pedidos/{id}

---
+++ src/main/java/com/ednilson/pedidos/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.ednilson.pedidos.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ednilson.pedidos.domain.Pedido;
+import com.ednilson.pedidos.repositories.PedidoRepository;
+import com.ednilson.pedidos.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: "+ Pedido.class.getName()));
+	}
+}
