358586e967136b60dd4678b00a334e8629f6b87e
ednilson <ednilsonpalhares@hotmail.com>
2020-03-08 21:46:14 -0300

    Inserindo pedido

--- src/main/java/com/ednilson/pedidos/resources/PedidoResource.java
+++ src/main/java/com/ednilson/pedidos/resources/PedidoResource.java
@@ -1,16 +1,23 @@
 package com.ednilson.pedidos.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ednilson.pedidos.domain.Categoria;
 import com.ednilson.pedidos.domain.Pedido;
+import com.ednilson.pedidos.dto.CategoriaDTO;
 import com.ednilson.pedidos.dto.PedidoDTO;
 import com.ednilson.pedidos.services.PedidoService;
 
@@ -29,10 +36,13 @@ public class PedidoResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<PedidoDTO>> findAll() {
-		List<Pedido> list = service.findAll();
-		List<PedidoDTO> listDto = list.stream().map(obj -> new PedidoDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/ednilson/pedidos/services/PedidoService.java
+++ src/main/java/com/ednilson/pedidos/services/PedidoService.java
@@ -1,12 +1,18 @@
 package com.ednilson.pedidos.services;
 
-import java.util.List;
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ednilson.pedidos.domain.ItemPedido;
+import com.ednilson.pedidos.domain.PagamentoComBoleto;
 import com.ednilson.pedidos.domain.Pedido;
+import com.ednilson.pedidos.domain.enums.EstadoPagamento;
+import com.ednilson.pedidos.repositories.ItemPedidoRepository;
+import com.ednilson.pedidos.repositories.PagamentoRepository;
 import com.ednilson.pedidos.repositories.PedidoRepository;
 import com.ednilson.pedidos.services.exception.ObjectNotFoundException;
 
@@ -16,13 +22,43 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagaRepo;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: "+ Pedido.class.getName()));
 	}
 	
-	public List<Pedido> findAll(){
-		return repo.findAll();
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagaRepo.save(obj.getPagamento());
+		
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
